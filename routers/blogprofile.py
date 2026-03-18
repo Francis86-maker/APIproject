851cad35b711118ca0e55fbdca54a8e9ee49fd41
Francis86-maker <francismukabya@gmail.com>
2026-03-18 05:35:05 +0300

    Add endpoint to get current user's blog
    Implement endpoint to retrieve current user's blog.

---
+++ routers/blogprofile.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends, HTTPException 
+from ..databasemodels import get_db
+from ..models import Blog
+from . import models 
+from auth import get_active_user
+
+router = APIRouter()
+
+@router.get('/blog/me')
+def get_blogs(current_user: Blog=Depends(get_active_user), db: Session=Depends(get_db)):
+  blog = db.query(models.User).filter(models.User.id == request.id)
+  if not blog:
+    raise HTTPException(atatus_code=404, detail="blog not found")
+  return blog
